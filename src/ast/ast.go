049085a6b648b14b8da22adfac3a719de6e5f865
elbehery <melbeher@redhat.com>
2022-03-31 13:17:07 +0200

    parser init

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -1,15 +1,9 @@
 package ast
 
-import (
-	"bytes"
+import "playground/go-interpreter/src/token"
 
-	"../token"
-)
-
-// interfaces
 type Node interface {
 	TokenLiteral() string
-	String() string
 }
 
 type Statement interface {
@@ -22,9 +16,8 @@ type Expression interface {
 	expressionNode()
 }
 
-// AST Root Node Type
 type Program struct {
-	Statements []Statement // tokens contained within statements
+	Statements []Statement
 }
 
 func (p *Program) TokenLiteral() string {
@@ -35,104 +28,25 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
-func (p *Program) String() string {
-	var out = bytes.Buffer{}
-
-	for _, stmt := range p.Statements {
-		out.WriteString(stmt.String())
-	}
-
-	return out.String()
-}
-
-// Let Node Type
 type LetStatement struct {
-	Token token.Token // token.LET
+	Token token.Token
 	Name  *Identifier
 	Value Expression
 }
 
-func (ls *LetStatement) statementNode()       {}
-func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
-func (ls *LetStatement) String() string {
-	var out = bytes.Buffer{}
-	out.WriteString(ls.TokenLiteral() + " ")
-	out.WriteString(ls.Name.String())
-	out.WriteString(" = ")
-	if ls.Value != nil {
-		out.WriteString(ls.Value.String())
-	}
-	out.WriteString(";")
-	return out.String()
+func (l *LetStatement) TokenLiteral() string {
+	return l.Token.Literal
 }
 
-// Identifier is Expression-Impl
+func (l *LetStatement) statementNode() {}
+
 type Identifier struct {
-	Token token.Token // token.IDENT
+	Token token.Token
 	Value string
 }
 
-func (i *Identifier) expressionNode()      {}
-func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
-func (i *Identifier) String() string {
-	return i.Value
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
 }
 
-type ReturnStatement struct {
-	Token token.Token
-	Value Expression
-}
-
-func (rs *ReturnStatement) statementNode()       {}
-func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
-func (rs *ReturnStatement) String() string {
-	var out = bytes.Buffer{}
-	out.WriteString(rs.TokenLiteral() + " ")
-	if rs.Value != nil {
-		out.WriteString(rs.Value.String())
-	}
-	out.WriteString(";")
-	return out.String()
-}
-
-type ExpressionStatement struct {
-	Token token.Token
-	Value Expression
-}
-
-func (es *ExpressionStatement) statementNode()       {}
-func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
-func (es *ExpressionStatement) String() string {
-	if es.Value != nil {
-		return es.Value.String()
-	}
-	return ""
-}
-
-type IntegerLiteral struct {
-	Token token.Token
-	Value int64
-}
-
-func (il *IntegerLiteral) expressionNode()      {}
-func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
-func (il *IntegerLiteral) String() string       { return il.Token.Literal }
-
-type PrefixExpression struct {
-	Token    token.Token
-	Operator string
-	Right    Expression
-}
-
-func (pe *PrefixExpression) expressionNode()      {}
-func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
-func (pe *PrefixExpression) String() string {
-	var out bytes.Buffer
-
-	out.WriteString("(")
-	out.WriteString(pe.Operator)
-	out.WriteString(pe.Right.String())
-	out.WriteString(")")
-
-	return out.String()
-}
+func (i *Identifier) expressionNode() {}
--- src/parser/parser_test.go
+++ src/parser/parser_test.go
@@ -1,204 +1,65 @@
 package parser
 
 import (
+	"playground/go-interpreter/src/ast"
+	"playground/go-interpreter/src/lexer"
 	"testing"
-
-	"../ast"
-	"../lexer"
 )
 
 func TestLetStatement(t *testing.T) {
 	input := `
-						let x = 5;
-						let y = 10;
-						let foobar = 838383; `
-	l := lexer.NewLexer(input)
-	p := NewParser(l)
+   let x = 5;
+   let y = 10;
+   let foobar = 838383;
+   `
+
+	l := lexer.New(input)
+	p := New(l)
 
 	program := p.ParseProgram()
-	checkParserErrors(t, p)
-
 	if program == nil {
-		t.Fatalf("ParseProgram() returned nil")
+		t.Fatal("ParseProgram() returned nil")
 	}
 	if len(program.Statements) != 3 {
-		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+		t.Fatalf("expected %v, but got %v statements instead", 3, len(program.Statements))
 	}
 
-	tests := []struct {
-		expectedIdentifier string
+	testCases := []struct {
+		ident string
 	}{
 		{"x"},
 		{"y"},
 		{"foobar"},
 	}
-	for i, tt := range tests {
+
+	for i, tt := range testCases {
 		stmt := program.Statements[i]
-		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+		if !testLetStatement(t, stmt, tt.ident) {
 			return
 		}
 	}
 }
 
-func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
-	if s.TokenLiteral() != "let" {
-		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+func testLetStatement(t *testing.T, stmt ast.Statement, ident string) bool {
+	if stmt.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let'. got=%q", stmt.TokenLiteral())
 		return false
 	}
-	letStmt, ok := s.(*ast.LetStatement)
+
+	s, ok := stmt.(*ast.LetStatement)
 	if !ok {
 		t.Errorf("s not *ast.LetStatement. got=%T", s)
 		return false
 	}
-	if letStmt.Name.Value != name {
-		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+
+	if s.Name.Value != ident {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", ident, s.Name.Value)
 		return false
 	}
-	if letStmt.Name.TokenLiteral() != name {
-		t.Errorf("s.Name not '%s'. got=%s", name, letStmt.Name)
+
+	if s.Name.TokenLiteral() != ident {
+		t.Errorf("s.Name not '%s'. got=%s", ident, s.Name)
 		return false
 	}
 	return true
-}
-
-func checkParserErrors(t *testing.T, p *Parser) {
-	errs := p.Errors()
-	if len(errs) == 0 {
-		return
-	}
-
-	t.Errorf("Parser had %d errors", len(errs))
-	for _, msg := range errs {
-		t.Errorf("Parser Error : %q ", msg)
-	}
-
-	t.FailNow()
-}
-
-func TestReturnStatement(t *testing.T) {
-	input := `
-						return 5;
-						return 2;
-						return -19;
-					`
-	l := lexer.NewLexer(input)
-	p := NewParser(l)
-
-	program := p.ParseProgram()
-	checkParserErrors(t, p)
-
-	exp := 3
-	if len(program.Statements) != exp {
-		t.Fatalf("program statement contains %d , but expected %d", len(program.Statements), exp)
-	}
-
-	for _, stmt := range program.Statements {
-		rtrn, ok := stmt.(*ast.ReturnStatement)
-		if !ok {
-			t.Errorf("stmt Type is not an *ast.ReturnStatement, but %T", stmt)
-			continue
-		}
-
-		if rtrn.TokenLiteral() != "return" {
-			t.Errorf("stmt Value is not 'return', but %v", rtrn.TokenLiteral())
-		}
-	}
-}
-
-func TestIdentifierExpression(t *testing.T) {
-	input := "foobar;"
-	l := lexer.NewLexer(input)
-	p := NewParser(l)
-	program := p.ParseProgram()
-	checkParserErrors(t, p)
-
-	if len(program.Statements) != 1 {
-		t.Fatalf("Program statements size is wrong, got  %v", len(program.Statements))
-	}
-	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
-	if !ok {
-		t.Fatalf("statment type is not ExpressionStatement, got %T", program.Statements[0])
-	}
-	id, ok := stmt.Value.(*ast.Identifier)
-	if !ok {
-		t.Fatalf("ExpressionStatement value is not Identifier, got %T", stmt.Value)
-	}
-	if id.Value != "foobar" {
-		t.Fatalf("identifier's Value is wrong, expected: %v, but got: %v", "foobar", id.Value)
-	}
-	if id.TokenLiteral() != "foobar" {
-		t.Fatalf("identifier's TokenLiteral() is wrong, expected: %v, but got: %v", "foobar", id.Value)
-	}
-}
-
-func TestIntegralExpression(t *testing.T) {
-	input := "1;"
-	lexer := lexer.NewLexer(input)
-	parser := NewParser(lexer)
-	program := parser.ParseProgram()
-	checkParserErrors(t, parser)
-
-	if len(program.Statements) != 1 {
-		t.Fatalf("program length is wrong, expected %v, got %v", 1, len(program.Statements))
-	}
-	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
-	if !ok {
-		t.Fatalf("program statment[0] type is wrong. expected: "+
-			"*ast.ExpressionStatement, got: %T", program.Statements[0])
-	}
-	val, ok := stmt.Value.(*ast.IntegerLiteral)
-	if !ok {
-		t.Fatalf("expression type is wrong. expected IntegerLiteral, got %T", stmt.Value)
-	}
-	if val.Value != 1 {
-		t.Fatalf("expression's value is wrong, expected 1, got %v", val.Value)
-	}
-	if val.TokenLiteral() != "1" {
-		t.Fatalf("expression's TokenLiteral() is wrong, expected 1, got %v", val.TokenLiteral())
-	}
-}
-
-func TestPrefixExpression(t *testing.T) {
-	prefixTests := []struct {
-		input    string
-		operator string
-		value    int64
-	}{
-		{"!5", "!", 5},
-		{"-15", "-", 15},
-	}
-
-	for _, tt := range prefixTests {
-		l := lexer.NewLexer(tt.input)
-		p := NewParser(l)
-		program := p.ParseProgram()
-		checkParserErrors(t, p)
-
-		if len(program.Statements) != 1 {
-			t.Fatalf("program's number of statements is wrong! expected: 1, got %v", len(program.Statements))
-		}
-		stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
-		if !ok {
-			t.Fatalf("program.statement[0] type is wrong. expected *ast.ExpressionStatement, got %T", program.Statements[0])
-		}
-		prefix, ok := stmt.Value.(*ast.PrefixExpression)
-		if !ok {
-			t.Fatalf("program's expression type is wrong. expected PrefixExpression, got %T", stmt.Value)
-		}
-		if prefix.Operator != tt.operator {
-			t.Fatalf("prefix expression's operator parsing is wrong."+
-				"expected %v, got %v", tt.operator, prefix.Operator)
-		}
-
-		// test right expression
-		iVal, ok := prefix.Right.(*ast.IntegerLiteral)
-		if !ok {
-			t.Fatalf("prefix expression's operand is wrong."+
-				"expected *ast.IntegerLiteral, got %T", prefix.Right)
-		}
-		if iVal.Value != tt.value {
-			t.Fatalf("prefix expression's operand is wrong."+
-				"expected %v, got %v", tt.value, iVal.Value)
-		}
-	}
 }
